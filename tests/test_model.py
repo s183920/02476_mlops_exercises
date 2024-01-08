1b8e18221c6a97c690887dabe0bac2d9ee584ae1
Lukas Leindals <s183920@student.dtu.dk>
2024-01-08 10:58:42 +0100

    skip model test if no model is found

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,17 +1,17 @@
 from tests import _PATH_DATA, _PROJECT_ROOT
 import torch
 import os
-from mlops_exercises.data.data import mnist
+import pytest
 
+MODEL_PATH = os.path.join(_PROJECT_ROOT, "models", "trained_model.pt")
 
+@pytest.mark.skipif(not os.path.exists(MODEL_PATH), reason="Data files not found")
 def test_model():
 
-    model = torch.load(os.path.join(_PROJECT_ROOT, "models", "trained_model.pt"), map_location=torch.device('cpu'))
-    train_loader, test_loader = mnist()
+    model = torch.load(MODEL_PATH, map_location=torch.device('cpu'))
     
-    pred = model(next(iter(train_loader))[0])
-    assert pred.shape == (5000, 10), "Prediction should have shape (5000, 10) but has {}".format(pred.shape)
+    x = torch.randn(5000, 1, 28, 28)
     
-    pred = model(next(iter(test_loader))[0])
+    pred = model(x)
     assert pred.shape == (5000, 10), "Prediction should have shape (5000, 10) but has {}".format(pred.shape)
     
